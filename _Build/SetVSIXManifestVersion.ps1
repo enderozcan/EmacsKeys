a46013916126c466f2b5a249141222abdd3104af
lpubsppop01 <lpubsppop01@gmail.com>
2017-06-02 23:59:57 +0900

    Copy AssemblyVersion to vsixmanifest on PreBuild

---
+++ _Build/SetVSIXManifestVersion.ps1
@@ -0,0 +1,10 @@
+﻿$scriptDirPath = Split-Path $MyInvocation.MyCommand.Path -Parent
+$assemblyInfoPath = Join-Path $scriptDirPath "..\Properties\AssemblyInfo.cs"
+$assemblyVersionLine = Get-Content $assemblyInfoPath | Select-String AssemblyVersion | Select-Object -First 1
+$assemblyVersion = $assemblyVersionLine -replace '.*AssemblyVersion\("(.*)"\).*', '$1'
+$manifestFilePath = Join-Path $scriptDirPath "..\source.extension.vsixmanifest"
+$manifestXML = [xml](Get-Content $manifestFilePath)
+$identityElement = $manifestXML.SelectSingleNode("//*[local-name(.) = 'Identity']")
+
+$identityElement.Version = $assemblyVersion
+$manifestXML.Save($manifestFilePath)
